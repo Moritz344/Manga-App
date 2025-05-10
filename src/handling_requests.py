e28db019a00dd5519896c719c9ea08550812c1a3
Moritz344 <moritzmaier34@gmail.com>
2025-05-10 20:31:11 +0200

    feat: working on ui layout

--- src/handling_requests.py
+++ src/handling_requests.py
@@ -6,8 +6,26 @@ import os
 # -- Manga Search Engine
 
 base_url = "https://api.mangadex.org"
-manga_title = None#manga_title #input("Manga title: ")
+manga_title = None
 
+def search_manga_result(manga_title="Frieren"):
+    r = requests.get(f"{base_url}/manga?title={manga_title}")
+    manga_data = r.json()
+
+    results = []
+
+    if r.status_code == 200:
+        for index in range(0,12):
+            print(index)
+            try:
+                name = manga_data["data"][index]["attributes"]["title"]["en"]
+            except Exception as e:
+                print("DEBUG:",e)
+                print("there are less than 11")
+            print(name)
+
+            results.append(name)
+    return results
 
 def get_manga_title(manga_title):
     try:
@@ -24,13 +42,10 @@ def get_manga_title(manga_title):
         print("DEBUG: ",e)
         print("Does this manga exist?")
 
-#manga_id = get_manga_title()
-try:
+def get_manga_chapters(manga_id,):
     feed_url = f"{base_url}/manga/{manga_id}/feed?translatedLanguage[]=en&order[chapter]=asc"
     feed_response = requests.get(feed_url)
-except Exception as e:
-    print(e)
-def get_manga_chapters(feed_response,):
+
     if feed_response.status_code == 200:
         chapters = feed_response.json()["data"]
         
@@ -45,13 +60,13 @@ def get_manga_chapters(feed_response,):
         first_chapter = chapters[0]
         chapter_1_id = first_chapter["id"] 
         chapter_1_num = first_chapter["attributes"].get("chapter") 
-
+        
+        print("Chapter id:",chapter_1_id,"Chapter num",chapter_1_num)
 
     return chapter_id,chapter_number,chapter_1_id,chapter_1_num
-#chapter_id,chapter_number,chapter_1_id,chapter_1_num = get_manga_chapters(feed_response)
 
 
-def get_server_data():
+def get_server_data(chapter_id):
     server_response = requests.get(f"{base_url}/at-home/server/{chapter_id}")
     if server_response.status_code == 200:
         server_data = server_response.json()
@@ -61,10 +76,9 @@ def get_server_data():
         pages = server_data["chapter"]["data"]
     
     return pages,host,chapter_hash
-#pages,host,chapter_hash = get_server_data()
 
 
-def downloading_chapters(pages,chapter_1_num):
+def downloading_chapters(pages,chapter_1_num,manga_title,host,chapter_hash):
     chapter_1_num = int(chapter_1_num)
     for num in range(chapter_1_num):
         num += 1
@@ -78,12 +92,11 @@ def downloading_chapters(pages,chapter_1_num):
                 with open(f"{folder_path}/{page}","wb") as file:
                     file.write(image_response.content)
             print(f"Downloaded {len(pages)} pages.")
-    else:
-        print("Path already exists.")
+        else:
+            print("Path already exists.")
     
     
 
-#downloading_chapters(pages,chapter_1_num)
 
 
 
--- src/ui.py
+++ src/ui.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
 import tkinter as tk
-from handling_requests import get_manga_title
+from handling_requests import get_manga_title,get_manga_chapters,get_server_data,downloading_chapters
+from handling_requests import search_manga_result
 import requests
 
 window = ctk.CTk()
@@ -13,6 +14,11 @@ def test_frame(window,manga_title):
     def get_title():
         manga_title = search_field.get()
         CollectMangaInfos(manga_title)
+    def get_manga_with_name():
+        manga_title = search_field.get()
+        print(manga_title)
+        c = DisplayMangaInfos(manga_title,main_frame)
+        c.display_mangas()
 
     
     entry_frame = ctk.CTkFrame(window,width=1200,height=100,)
@@ -21,19 +27,66 @@ def test_frame(window,manga_title):
     search_field = ctk.CTkEntry(entry_frame,width=900,height=70,font=(None,30))
     search_field.pack(side="top",padx=0,pady=10)
 
-    search_btn = ctk.CTkButton(entry_frame,text="Search",font=(None,20),command=get_title)
-    search_btn.pack(side="top")
+    download_btn = ctk.CTkButton(entry_frame,text="Download",font=(None,20),command=get_title)
+    download_btn.pack(side="top")
+
+    search_btn = ctk.CTkButton(entry_frame,text="Search",font=(None,20),command=get_manga_with_name)
+    search_btn.pack(side="top",padx=0,pady=0)
 
 
-    t = ctk.CTkFrame(window,width=1000,height=800)
-    t.pack(side="top",padx=0,pady=10)
+    main_frame = ctk.CTkFrame(window,width=1000,height=800,fg_color="transparent")
+    main_frame.pack(side="top",padx=0,pady=10,expand=True)
     
 
     return manga_title
 
 manga_title = test_frame(window,manga_title)
 
+
+class DisplayMangaInfos(object):
+    def __init__(self,manga_title,window):
+        self.manga_title = manga_title
+        self.window = window
+        #self.result = search_manga_result(manga_title)
+
+        #self.manga_num = len(self.result)
+
+    def display_mangas(self):
+        for widget in self.window.winfo_children():
+            widget.destroy()
+        grid_container = ctk.CTkFrame(self.window, width=1000, height=800,fg_color="transparent")
+        grid_container.grid(row=0, column=0, padx=20, pady=20)
+        grid_container.grid_propagate(False)  
+
+        scrollable_frame = ctk.CTkScrollableFrame(master=grid_container, width=1000, height=500)
+        scrollable_frame.pack()
+
+        for i in range(3):  # 3 Spalten
+            grid_container.grid_columnconfigure(i, weight=1)
+        for i in range(3):  # 3 Zeilen
+            grid_container.grid_rowconfigure(i, weight=1)
+
+        for i in range(10):
+            row = i // 1
+            col = i % 1
+
+            block = ctk.CTkFrame(scrollable_frame,width=1000,height=200)
+            block.grid(row=row,column=col,padx=20,pady=20)
+
+
+            block_label = ctk.CTkLabel(block,text=f"{i}",font=(None,30))#text=f"{self.result[i]}",font=(None,15))
+            block_label.place(x=0,y=0)
+
+
 class CollectMangaInfos(object):
     def __init__(self,manga_title):
-        manga_id = get_manga_title(manga_title)
+        try:
+            manga_id = get_manga_title(manga_title)
+            chapter_id,chapter_number,chapter_1_id,chapter_1_num = get_manga_chapters(manga_id)
+            pages,host,chapter_hash = get_server_data(chapter_id)
+            downloading_chapters(pages,chapter_1_num,manga_title,host,chapter_hash)
+        except Exception as e:
+            print(e)
+
+
 window.mainloop()
