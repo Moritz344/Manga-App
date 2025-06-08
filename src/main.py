84082b5304a43a9a27ae86981b9eeab9d2f83c96
Moritz344 <moritzmaier34@gmail.com>
2025-06-08 15:46:51 +0200

    feat: added todos

--- src/main.py
+++ src/main.py
@@ -7,10 +7,10 @@ from PIL import Image
 from display_manga import DisplayMangaInfos,ReadMangaScreen,main_window_frame
 from ctk_components import CTkLoader
 
-#TODO: bug bei readmangascreen wenn ich zu dem search screen gehe
+#TODO: begrenze description länge von manga
+#TODO: manga status
 #TODO: json datei benutzen für settings
 #TODO: mangas speichern in sql lite
-#TODO: ChapterView ui verbessern siehe figma
 #TODO: Downloaded Manga Page -> Wo man seine heruntergeladenen Mangas anschauen kann also history oder so
 #TODO: Markierte Mangas Manga Page
 #TODO: hacker theme 
@@ -25,7 +25,6 @@ manga_title = None
 manag_title = main_window_frame(window,manga_title)
 #loader = CTkLoader(master=window, opacity=0.8, width=40, height=40)
 #window.after(500, loader.stop_loader) 
-
 
 
 
