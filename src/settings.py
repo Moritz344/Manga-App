b3f79dc7712d08bc397f787c04504d8c73d67e43
Moritz344 <moritzmaier34@gmail.com>
2025-06-18 18:22:27 +0200

    feat: added text color

--- src/settings.py
+++ src/settings.py
@@ -12,6 +12,7 @@ def get_data_from_json() -> str:
             color_green = data["settings"]["color_green"]
             history = data["user_var"][f"{manga_name}"]
             len_history = data["user_var"]
+            text_color = data["settings"]["text_color"]
 
             dark_charcoal = data["settings"]["dark charcoal"]
             electric_blue = data["settings"]["electric Blue"]
@@ -21,7 +22,7 @@ def get_data_from_json() -> str:
             chapter_download = data["settings"]["chapter_download"]
 
 
-            return button_color,button_hover_color,block_color,color_blue,color_green,history,manga_name,len_history,dark_charcoal,electric_blue,background,font_size,manga_location,chapter_download
+            return button_color,button_hover_color,block_color,color_blue,color_green,history,manga_name,len_history,dark_charcoal,electric_blue,background,font_size,manga_location,chapter_download,text_color
             
-button_color,button_hover_color,block_color,color_blue,color_green,history,manga_name,len_history,dark_charcoal,electric_blue,background,font_size,manga_location,chapter_download = get_data_from_json()
+button_color,button_hover_color,block_color,color_blue,color_green,history,manga_name,len_history,dark_charcoal,electric_blue,background,font_size,manga_location,chapter_download,text_color = get_data_from_json()
 
