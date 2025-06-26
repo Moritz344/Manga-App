04212ea90cc46c3c5ed67e35ec48a96ef03903cd
Moritz344 <moritzmaier34@gmail.com>
2025-06-26 18:54:18 +0200

    feat: added functions for getting length of pages in a chapter

--- src/get_downloaded_mangas.py
+++ src/get_downloaded_mangas.py
@@ -1,6 +1,8 @@
+import os
+from json_utils.settings import mangas_downloaded,manga_location
+import functools
 
-from json_utils.settings import mangas_downloaded
-
+@functools.lru_cache
 def downloaded_mangas() -> list:
     installed_mangas = []
 
@@ -11,4 +13,20 @@ def downloaded_mangas() -> list:
     return installed_mangas
 
 
-downloaded_mangas()
+@functools.lru_cache
+def get_pages_from_downloaded_mangas(manga_title,chapter_number) -> int:
+    # return the length of pages the given chapter has
+
+    path = f"{manga_location}/{manga_title}"
+    chapter_list = os.listdir(f"{path}/Chapter_{chapter_number}")
+    chapter_count = len(chapter_list)
+
+    pages_len = 0
+
+    x = os.listdir(f"{path}/Chapter_{chapter_number}")
+    pages_len = len(x)
+
+
+    return pages_len
+
+
